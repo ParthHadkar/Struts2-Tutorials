575323670df692ec8fe713f0a2cc6b63e54b8ae6
s-pc <s-pc@localhost>
2020-10-22 03:17:59 +0530

    P1 commited

---
+++ P01StrutsDemo/src/main/java/com/p01strutsdemo/model/Login.java
@@ -0,0 +1,32 @@
+package com.p01strutsdemo.model;
+
+public class Login {
+	private String username,password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String execute() {
+		String result = "";
+		if(username.equals("Admin") && password.equals("Admin")) {
+			result ="success";
+		}
+		else {
+			result ="error";
+		}
+		return result;
+	}
+}
